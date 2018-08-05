117b2f64bceb0c167bb5b044ae08e8c05718003b
arcuri82 <arcuri82@gmail.com>
2018-08-05 11:17:26 +0200

    fixed issue with missing key attribute

--- les03/spa-components-react/src/container.jsx
+++ les03/spa-components-react/src/container.jsx
@@ -21,22 +21,28 @@ export class Container extends React.Component {
                         First of all, we need to instantiate Counter components.
                         This is done by directly using JSX tag <Counter>, based
                         on the name of the component.
+
                         We want to do it n times.
                         So we start to create an Array(n).
                         But such array created with the Array() constructor has
                         no property. To create an array with properties from an
                         array of length n, we use "from()", otherwise the map()
                         function will NOT work. Confusing, isn't it?
+
                         On such array, we apply map(), which takes 2 inputs: the
                         element "e" we iterate on, but also the index "i" in the
                         iteration.
                         What we finally get from the map is an array of <Counter>
                         components, each one with different attribute "name".
+
                         Besides single components, in JSX we can display arrays/lists,
                         which will just concatenate the generated HTML.
+                        However, we need to specify a "key" attribute that must
+                        be unique. This is needed for React to optimize when components
+                        need to be re-rendered
                      */
                     Array.from(Array(n))
-                        .map((e,i) => <Counter name={i}/>)
+                        .map((e,i) => <Counter name={i} key={"counter_unique_key_" + i}/>)
                 }
             </div>
         );
