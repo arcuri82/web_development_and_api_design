0392dd48c03873fe0127000bc78346d25e296c42
arcuri82 <arcuri82@gmail.com>
2018-08-07 09:43:31 +0200

    React tests for Connect4

--- les03/connect4/tests/board.test.jsx
+++ les03/connect4/tests/board.test.jsx
@@ -1,26 +1,208 @@
-// const React = require('react');
-// const jsdom = require('jsdom');
-// const Adapter = require('enzyme-adapter-react-16');
-//
-// const { mount, configure } = require('enzyme');
-// configure({ adapter: new Adapter() });
-//
-// global.document = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
-// global.window = document.defaultView;
-// global.navigator = {userAgent: 'node.js'};
-
-
-const {Board} = require("../src/board");
 const React = require('react');
 const { mount } = require('enzyme');
+const {Board} = require("../src/board");
 
+/*
+    Useful links:
 
+    https://github.com/airbnb/enzyme/blob/master/docs/api/mount.md
+    https://www.w3schools.com/cssref/css_selectors.asp
+ */
 
 test("Visualize board", ()=> {
 
-    const wrapper = mount(<Board isGameOn={true}/>);
+    const driver = mount(<Board isGameOn={true}/>);
 
-    const divs = wrapper.find('.cell');
+    const divs = driver.find('.cell');
 
     expect(divs.length).toEqual(42);
+});
+
+
+const clickTopRow = function(driver, column){
+
+    const div = driver.find('.cell').at(column);
+
+    div.simulate('click');
+};
+
+test("X wins horizontal", () => {
+
+    let result = 0;
+
+    let handler = (r,x) => {result = r;};
+
+    const driver = mount(<Board isGameOn={true} resultHandler={handler}/>);
+
+    expect(result).toBe(0); //game is on
+
+    clickTopRow(driver, 0); // X
+    clickTopRow(driver, 0); // O
+
+    clickTopRow(driver, 1);
+    clickTopRow(driver, 1);
+
+    clickTopRow(driver, 2);
+    clickTopRow(driver, 2);
+
+    expect(result).toBe(0); // game still on
+
+    clickTopRow(driver, 3);
+
+    /*
+       ooo
+       xxxx
+     */
+
+    expect(result).toBe(1); // X won
+});
+
+
+test("O wins horizontal", () => {
+
+    let result = 0;
+
+    let handler = (r,x) => {result = r;};
+
+    const driver = mount(<Board isGameOn={true} resultHandler={handler}/>);
+
+    expect(result).toBe(0); //game is on
+
+    clickTopRow(driver, 6); // X
+    clickTopRow(driver, 0); // O
+
+    clickTopRow(driver, 6); // X in column up to 3
+    clickTopRow(driver, 1);
+
+    clickTopRow(driver, 6);
+    clickTopRow(driver, 2);
+
+    clickTopRow(driver, 5); // X not winning
+
+    expect(result).toBe(0); // game still on
+
+    clickTopRow(driver, 3);
+
+    /*
+              x
+              x
+        0000 xx
+     */
+
+    expect(result).toBe(2); // Y won
+});
+
+
+
+test("X wins vertical", () => {
+
+    let result = 0;
+
+    let handler = (r,x) => {result = r;};
+
+    const driver = mount(<Board isGameOn={true} resultHandler={handler}/>);
+
+    expect(result).toBe(0); //game is on
+
+    clickTopRow(driver, 0); // X
+    clickTopRow(driver, 1); // O
+
+    clickTopRow(driver, 0);
+    clickTopRow(driver, 1);
+
+    clickTopRow(driver, 0);
+    clickTopRow(driver, 1);
+
+    expect(result).toBe(0); // game still on
+
+    clickTopRow(driver, 0);
+
+    /*
+
+        x
+        xo
+        xo
+        xo
+     */
+
+    expect(result).toBe(1); // X won
+});
+
+
+test("X wins up diagonal", () => {
+
+    let result = 0;
+
+    let handler = (r,x) => {result = r;};
+
+    const driver = mount(<Board isGameOn={true} resultHandler={handler}/>);
+
+    expect(result).toBe(0); //game is on
+
+    clickTopRow(driver, 0); // X
+    clickTopRow(driver, 1); // O
+
+    clickTopRow(driver, 1);
+    clickTopRow(driver, 2);
+
+    clickTopRow(driver, 2);
+    clickTopRow(driver, 3);
+
+    clickTopRow(driver, 2);
+    clickTopRow(driver, 3);
+
+    clickTopRow(driver, 3);
+    clickTopRow(driver, 0);
+
+    expect(result).toBe(0); // game still on
+
+    clickTopRow(driver, 3);
+
+    /*
+           x
+          xx
+        oxxo
+        xooo
+     */
+
+    expect(result).toBe(1); // X won
+});
+
+
+test("O wins down diagonal", () => {
+
+    let result = 0;
+
+    let handler = (r,x) => {result = r;};
+
+    const driver = mount(<Board isGameOn={true} resultHandler={handler}/>);
+
+    expect(result).toBe(0); //game is on
+
+    clickTopRow(driver, 1); // X
+    clickTopRow(driver, 0); // O
+
+    clickTopRow(driver, 2);
+    clickTopRow(driver, 3);
+
+    clickTopRow(driver, 1);
+    clickTopRow(driver, 1);
+
+    clickTopRow(driver, 0);
+    clickTopRow(driver, 2);
+
+    clickTopRow(driver, 0);
+
+    expect(result).toBe(0); // game still on
+
+    clickTopRow(driver, 0);
+
+    /*
+       o
+       xo
+       xxo
+       oxxo
+     */
+
+    expect(result).toBe(2); // O won
 });
