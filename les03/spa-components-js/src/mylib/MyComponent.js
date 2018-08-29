0c881a218598141dfbd1c3e9c4246a7442ad3d7e
arcuri82 <arcuri82@gmail.com>
2018-08-29 13:10:26 +0200

    some clarifications

--- les03/spa-components-js/src/mylib/MyComponent.js
+++ les03/spa-components-js/src/mylib/MyComponent.js
@@ -5,6 +5,7 @@ let id_counter = 0;
 export class MyComponent {
 
     constructor() {
+        //each component will have a unique id
         this.id = "MyComponent_id_" + id_counter;
         id_counter++;
     }
@@ -13,6 +14,7 @@ export class MyComponent {
 
         this.state = state;
 
+        //every time there is a state change, we render the components
         update();
     }
 
@@ -20,6 +22,17 @@ export class MyComponent {
         return this.id === id;
     }
 
+    /*
+        In the generated HTML for the component, we will need to be able to
+        register event handlers.
+        But those handlers need to refer to the specific component they belong to.
+        So, the generated JS in the page somehow needs to find the right component to
+        use its state.
+        We will use a global support function registered in MyDOM.js.
+
+        This works, but it has limitations: the method must not have input parameters,
+        and should be a method belonging to the class of the component.
+     */
     methodHandler(method) {
 
         const name = method.name;
--- les03/spa-components-js/src/mylib/MyDOM.js
+++ les03/spa-components-js/src/mylib/MyDOM.js
@@ -7,7 +7,11 @@
 let _component;
 let _htmlHook;
 
-
+/*
+    this method is used to "bind" a component to a specific HTML tag
+    in the home page.
+    The component will be rendered as a child of this tag.
+ */
 export const render = function(component, htmlHook){
 
     _component = component;
@@ -16,6 +20,19 @@ export const render = function(component, htmlHook){
     update();
 };
 
+/*
+    This is called internally by our library every time there is a state change
+    in any of components.
+    It will force the rerendering of the components.
+    However, it is very, very inefficient: for any change, the generation of HTML
+    is done for ALL the components in the tree, even for the ones that are not
+    changed.
+    Furthermore, once we get the HTML for the component, we change all of it on the browser,
+    and not only the parts that are different.
+    What could have been done is to use a "Virtual DOM": keep track of the HTML of the components and,
+    when we generate new HTML due to a state change, calculate a "diff" between the existing HTML
+    in the browser the and new one, and so then update (ie changes in the actual DOM) only the differences
+ */
 export const update = function(){
 
     const html = _component.render();
@@ -24,6 +41,19 @@ export const update = function(){
 };
 
 
+/*
+    This is a bit tricky. On the HTML generated for a component, we can
+    have JS code in the event handlers.
+    But such methods should be called on a class instance of that component,
+    otherwise it would not be able to use its state.
+    In the global scope, we store the root component we display.
+    So, we need to refer to specific components on the tree.
+    We give each generated component a unique id, and then have a global
+    function that takes as input the id and the name of a method in such
+    component we want to call.
+    Such method will find the right component in the tree, and then call
+    such function.
+ */
 export const callMethodOnComponent = function (id, method_name) {
 
     const target = findComponent(_component, id);
@@ -31,6 +61,16 @@ export const callMethodOnComponent = function (id, method_name) {
         throw "Could not find component with id: " + id;
     }
 
+    /*
+        This notation might seem confusing at first.
+        "target" is an object.
+        On an object, we can access its fields using the [] operator
+        specifying the name of the field we want,
+        eg obj["aname"] will return the value of the field called "aname"
+        in the object called "obj".
+        As in our case the value is a function, we can then call
+        such function with ().
+     */
     target[method_name]();
 };
 
