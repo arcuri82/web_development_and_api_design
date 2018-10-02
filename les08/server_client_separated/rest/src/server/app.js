d28dd0fcaf6db97c25e8c4c005700ce5c058a3b3
arcuri82 <arcuri82@gmail.com>
2018-10-02 14:53:55 +0200

    clarifications

--- les08/server_client_separated/rest/src/server/app.js
+++ les08/server_client_separated/rest/src/server/app.js
@@ -7,14 +7,34 @@ const app = express();
 //to handle JSON payloads
 app.use(bodyParser.json());
 
-//to enable CORS filters
+/*
+    to enable CORS filters. If we do not enable it, then the JS code
+    in bundle.js downloaded from "frontend" server (running on origin localhost:8080)
+    would not be allowed to do requests against this REST service.
+ */
 app.use(cors({
     origin: 'http://localhost:8080'
 }));
 
+/*
+    Here we define a series of endpoints, where HTTP will be routed to
+    different endpoints based on the HTTP verb (eg GET and POST) and the
+    path component of the URL, eg, "/books" when the URL is
+    "http://localhost:8080/books"
+ */
 
+/*
+    Return (ie GET) all the books.
+    Can filter by publication year using the query parameter "since".
+    The books will be returned as a list of JSON objects.
+ */
 app.get('/books', (req, res) => {
 
+    /*
+        Read the query parameters, if any, eg:
+
+        http://localhost:8080/books?since=2001
+     */
     const since = req.query["since"];
 
     if (since !== undefined && since !== null) {
@@ -25,7 +45,9 @@ app.get('/books', (req, res) => {
 });
 
 /*
-    Note the use of ":" to represent a variable placeholder
+    Note the use of ":" to represent a variable placeholder.
+    Here we return a specific book with a specific id, eg
+    "http://localhost:8080/books/42"
  */
 app.get('/books/:id', (req, res) => {
 
@@ -45,7 +67,9 @@ app.get('/books/:id', (req, res) => {
      */
 });
 
-
+/*
+    Handle HTTP DELETE request on a book specified by id
+ */
 app.delete('/books/:id', (req, res) => {
 
     const deleted = repository.deleteBook(req.params.id);
@@ -58,7 +82,11 @@ app.delete('/books/:id', (req, res) => {
     res.send();
 });
 
-
+/*
+    Create a new book. The id will be chosen by the server.
+    Such method should return the "location" header telling
+    where such book can be retrieved (ie its URL)
+ */
 app.post('/books', (req, res) => {
 
     const dto = req.body;
@@ -71,6 +99,10 @@ app.post('/books', (req, res) => {
 });
 
 
+/*
+    Handle PUT request, which completely replace the resource
+    with a new one
+ */
 app.put('/books/:id', (req, res) => {
 
     if(req.params.id !== req.body.id){
