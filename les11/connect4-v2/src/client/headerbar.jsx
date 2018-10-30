1b870830a82f57fcdf7c0b5929c896781c54ea15
Sara <saradalfalk@gmail.com>
2018-10-30 20:52:51 +0100

    Added style

--- les11/connect4-v2/src/client/headerbar.jsx
+++ les11/connect4-v2/src/client/headerbar.jsx
@@ -1,83 +1,88 @@
-import React from 'react';
-import {Link, withRouter} from 'react-router-dom';
-
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
 /*
     Just provide a header component for all pages, where we have a link to the
     home-page, and login/sing-up/logout buttons.
  */
-class HeaderBar extends React.Component{
+class HeaderBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
+    this.doLogout = this.doLogout.bind(this);
+  }
 
-        this.doLogout = this.doLogout.bind(this);
+  async doLogout() {
+    const url = "/api/logout";
+
+    let response;
+
+    try {
+      response = await fetch(url, { method: "post" });
+    } catch (err) {
+      alert("Failed to connect to server: " + err);
+      return;
     }
 
-
-    async doLogout(){
-
-        const url = "/api/logout";
-
-
-        let response;
-
-        try {
-            response = await fetch(url, {method: "post"});
-        } catch (err) {
-            alert("Failed to connect to server: "+ err);
-            return;
-        }
-
-        if(response.status !== 204){
-            alert("Error when connecting to server: status code "+ response.status);
-            return;
-        }
-
-        this.props.updateLoggedInUserId(null);
-        this.props.history.push('/');
+    if (response.status !== 204) {
+      alert("Error when connecting to server: status code " + response.status);
+      return;
     }
 
-    renderLoggedIn(userId){
-        return(
-            <div >
+    this.props.updateLoggedInUserId(null);
+    this.props.history.push("/");
+  }
 
-                <h3>Welcome {userId}!!!</h3>
+  renderLoggedIn(userId) {
+    return (
+      <div className="msgDiv">
+        <h3 className="notLoggedInMsg">
+          Welcome {userId}
+          !!!
+        </h3>
 
-                <div className="btn" onClick={this.doLogout}>Logout</div>
-            </div>
-        );
+        <div className="btn btnPartHeader" onClick={this.doLogout}>
+          Logout
+        </div>
+      </div>
+    );
+  }
+
+  renderNotLoggedIn() {
+    return (
+      <div className="msgDiv">
+        <div className="notLoggedInMsg">You are not logged in</div>
+        <div className="btnPartHeader">
+          <Link className="btn" to="/login">
+            LogIn
+          </Link>
+          <Link className="btn" to="/signup">
+            SignUp
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const userId = this.props.userId;
+
+    let content;
+    if (userId === null || userId === undefined) {
+      content = this.renderNotLoggedIn();
+    } else {
+      content = this.renderLoggedIn(userId);
     }
 
-    renderNotLoggedIn(){
-        return(
-            <div>
-                <p>You are not logged in</p>
-
-                <Link to="/login">LogIn</Link>
-                <Link to="/signup">SignUp</Link>
-            </div>
-
-        );
-    }
-
-    render(){
-        const userId = this.props.userId;
-
-        let content;
-        if(userId === null || userId === undefined){
-            content = this.renderNotLoggedIn();
-        } else {
-            content = this.renderLoggedIn(userId);
-        }
-
-        return(
-            <div className={"headerBar"}>
-                <Link to={"/"}>Home</Link>
-                {content}
-            </div>
-        );
-    }
+    return (
+      <div className={"headerBar"}>
+        <Link className="btn home" to={"/"}>
+          Home
+        </Link>
+        {content}
+      </div>
+    );
+  }
 }
 
 export default withRouter(HeaderBar);
--- les11/connect4-v2/src/client/home.jsx
+++ les11/connect4-v2/src/client/home.jsx
@@ -1,50 +1,47 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 export class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
+  render() {
+    const userId = this.props.userId;
+    const loggedIn = userId !== null && userId !== undefined;
 
-    render() {
+    return (
+      <div>
+        <div>
+          <h2>Connect Four Game</h2>
 
-        const userId = this.props.userId;
-        const loggedIn = (userId !== null && userId !== undefined);
+          <p>
+            Welcome to the Connect Four Game! In this game, you alternate with
+            an opponent in adding coins (X or O) into a 6x7 grid. The goal of
+            the game is to align 4 of your coins, either horizontally,
+            vertically or diagonally.
+          </p>
+          <p>
+            You can play either against an AI, or against other players online.
+            However, to play online, you need to create an account and be logged
+            in.
+          </p>
+        </div>
 
-
-        return (
-            <div>
-
-                <div>
-                    <h2>Connect Four Game</h2>
-
-                    <p>
-                        Welcome to the Connect Four Game!
-                        In this game, you alternate with an opponent in
-                        adding coins (X or O) into a 6x7 grid.
-                        The goal of the game is to align 4 of your coins,
-                        either horizontally, vertically or diagonally.
-                    </p>
-                    <p>
-                        You can play either against an AI, or against other
-                        players online.
-                        However, to play online, you need to create an account
-                        and be logged in.
-                    </p>
-                </div>
-
-                <div>
-                    <Link to={"/match/ai"} className={"btn"}>AI Match</Link>
-                    {loggedIn ?
-                        <Link to={"/match/online"} className={"btn"}>Online Match</Link>
-                        :
-                        //TODO
-                        <div>Online Match (need to log in)</div>
-                    }
-                </div>
-            </div>
-        );
-    }
+        <div className="btnPart">
+          <Link to={"/match/ai"} className={"btn"}>
+            AI Match
+          </Link>
+          {loggedIn ? (
+            <Link to={"/match/online"} className={"btn"}>
+              Online Match
+            </Link>
+          ) : (
+            //TODO
+            <div className="btnDeActive">Online Match</div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
