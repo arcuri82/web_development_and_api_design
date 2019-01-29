2d9b6ff50b82fa8948e38fc8ead53b63cac8e2bb
arcuri82 <arcuri82@gmail.com>
2019-01-29 13:55:31 +0100

    working on les07

---
+++ les07/server_client_together/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/client/index.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+        libraryTarget: 'var',
+        library: 'EntryPoint'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        contentBase: './public'
+    }
+};
