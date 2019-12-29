72bbd611c9cefbf2341bdcf36956a520e2f90386
dalSara <saradalfalk@gmail.com>
2019-12-29 20:22:31 +0100

    lesson 4

--- exercise-solutions/quiz-game/part-04/src/index.jsx
+++ exercise-solutions/quiz-game/part-04/src/index.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {Match} from "./match";
 
-
 const App = () => {
         return (
-            <div>
+            <>
                 <Match/>
-            </div>
+            </>
         );
 };
-
 
 ReactDOM.render(<App/>, document.getElementById("root"));
 
