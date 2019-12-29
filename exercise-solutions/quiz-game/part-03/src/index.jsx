a641604b159fc8a14e9031c9d282bdffe325337a
dalSara <saradalfalk@gmail.com>
2019-12-29 20:13:38 +0100

    lesson 3

--- exercise-solutions/quiz-game/part-03/src/index.jsx
+++ exercise-solutions/quiz-game/part-03/src/index.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
 
 
     renderAnswerTag(prefix, answer, correct) {
-        return <div className='gameBtn' onClick={() => this.handleClick(correct)}> {prefix + answer} </div>;
+        return <div className='answer' onClick={() => this.handleClick(correct)}> {prefix + answer} </div>;
     }
 
     render() {
@@ -32,13 +32,20 @@ class App extends React.Component {
         const quiz = this.state.quiz;
 
         return (
-            <div>
+            /*This <></> is a shorthand React fragment, full tag is <React.Fragment></React.Fragment>, 
+            the shorthand is just so much cleaner. 
+            It is usfull when we dont want another html tag, 
+            like a div to be added to the html three but need something that collects other elemnts. 
+            If you look at this from the development tools in your browser, it wont be visable. 
+            Doc: https://reactjs.org/docs/fragments.html
+            */
+            <>
                 <p className='question'>Question: {quiz.question} </p>
                 {this.renderAnswerTag("A: ", quiz.answers[0], quiz.indexOfRightAnswer === 0)}
                 {this.renderAnswerTag("B: ", quiz.answers[1], quiz.indexOfRightAnswer === 1)}
                 {this.renderAnswerTag("C: ", quiz.answers[2], quiz.indexOfRightAnswer === 2)}
                 {this.renderAnswerTag("D: ", quiz.answers[3], quiz.indexOfRightAnswer === 3)}
-            </div>
+            </>
         );
     }
 }
