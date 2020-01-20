62f8c27a895b13e685c3a535d4f785b5cbc3fb44
arcuri82 <arcuri82@gmail.com>
2020-01-20 14:59:44 +0100

    fixed comment

--- les03/spa-components-react/src/index.jsx
+++ les03/spa-components-react/src/index.jsx
@@ -8,11 +8,11 @@ const App = () => {
         Furthermore, it should be only a single tag with children (if any).
         If you want several tags at the same level (ie siblings), you need to put
         them inside a <div> for example.
-        If you dont want to add a tag to your html-three you can use a React fragment.
-        The full tag is <React.Fragment></React.Fragment> the short hand looks like this <></>.
-        If you look at this <></> from the development tools in your browser, it wont be visible.
-        Doc: https://reactjs.org/docs/fragments.html 
-         If you remove the <> and have the <h2> and <Container> one after the
+        If you do not want to add a tag to your html-tree, you can use a React fragment.
+        The full tag is <React.Fragment></React.Fragment>, whereas the shorthand looks like this <></>.
+        If you look at this <></> from the development tools in your browser, it will not be visible.
+        More info at: https://reactjs.org/docs/fragments.html
+        If you remove the <>, and have the <h2> and <Container> one after the
         other, you will get an error.
      */
   return (
