0caf8d01c72154429d21edebf3ac2e9bfd5c7fd6
arcuri82 <arcuri82@gmail.com>
2020-02-27 12:02:34 +0100

    clarifications on React-Router

--- les05/spa-routing-react/src/index.jsx
+++ les05/spa-routing-react/src/index.jsx
@@ -11,11 +11,31 @@ const App = () => {
 
     return (
         <BrowserRouter>
+            {/*
+                To use React-Router, the root component needs to be inside a BrowserRouter.
+                Then, we have a Switch, which is the same concept of "switch" in programming
+                languages (ie, equivalent to a chain of if/else).
+                Only one single Route will be active. Those are checked in order, from top
+                to bottom, based on the "path" attribute.
+                If such value does match the path element of the URL in the address bar of
+                the browser, then the "component" will be rendered in the switch, whereas all
+                the others will be ignored.
+
+                If for any reason you need to pass some properties to the rendered components,
+                then instead of using "component" (which does not accept JSX), you need to
+                use "render". This takes as input a function, whose input itself is the props
+                of this component (ie App), if any
+            */}
             <div>
                 <Switch>
                     <Route exact path="/first" component={First}/>
-                    <Route exact path="/second" component={Second}/>
+                    <Route exact path="/second" render={(props) => <Second />}/>
                     <Route exact path="/" component={Home}/>
+                    {/*
+                        Note that this last Route has no "path". If there was no match
+                        in any of the previous Route's paths, then this last one will
+                        act as "default"
+                    */}
                     <Route component={NotFound}/>
                 </Switch>
             </div>
