2a3110e7cb38a9c8a44cad120e6d3e6a11e499b1
Sara Dalfalk <saradalfalk@gmail.com>
2020-03-17 18:46:23 +0100

    added style

--- exercise-solutions/quiz-game/part-08/src/client/home.jsx
+++ exercise-solutions/quiz-game/part-08/src/client/home.jsx
@@ -1,54 +1,46 @@
 import React from "react";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 export class Home extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if (this.props.user) {
+      this.props.fetchAndUpdateUserInfo();
     }
+  }
 
-    componentDidMount() {
-        if(this.props.user) {
-            this.props.fetchAndUpdateUserInfo();
-        }
-    }
+  render() {
+    const user = this.props.user;
+    const loggedIn = user !== null && user !== undefined;
 
-    render() {
-        const user = this.props.user;
-        const loggedIn = user !== null && user !== undefined;
+    return (
+      <div className="main-content">
+        <h2 className="heading">Play the Quiz Game</h2>
 
-        return (
-            <div>
-                <div>
-                    <h2>Quiz Game</h2>
+        <p>
+          Welcome to the Quiz Game! In this game, you will get a series of
+          questions, each one with 4 possible answers. Only 1 out of the 4
+          answers is correct. If you answer wrongly to any of the questions, you
+          lose! You win if you manage to answer correctly to all questions.
+        </p>
 
-                    <p>
-                        Welcome to the Quiz Game!
-                        In this game, you will get a series of questions, each one with 4 possible answers.
-                        Only 1 out of the 4 answers is correct.
-                        If you answer wrongly to any of the questions, you lose!
-                        You win if you manage to answer correctly to all questions.
-                    </p>
-                </div>
-
-
-                {loggedIn ? (
-                    <div>
-                        <div className="btnPart">
-                            <Link to={"/match"} className={"btn"}>
-                                Play
-                            </Link>
-                        </div>
-                        <p>Victories: {user.victories}</p>
-                        <p>Defeats: {user.defeats}</p>
-                    </div>
-                ) : (
-                    <p>
-                        You need to log-in to start playing!
-                    </p>
-                )}
-
+        {loggedIn ? (
+          <div>
+            <Link to={"/match"} className={"play"}>
+              Play
+            </Link>
+            <div className="action">
+              <p>Victories: {user.victories}</p>
+              <p>Defeats: {user.defeats}</p>
             </div>
-        );
-    }
+          </div>
+        ) : (
+          <p>You need to log-in to start playing!</p>
+        )}
+      </div>
+    );
+  }
 }
