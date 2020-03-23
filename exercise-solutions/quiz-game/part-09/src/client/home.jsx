1d42911913ed91831000878fd4234bb6e50c1702
Sara Dalfalk <saradalfalk@gmail.com>
2020-03-23 17:44:06 +0100

    linting

--- exercise-solutions/quiz-game/part-09/src/client/home.jsx
+++ exercise-solutions/quiz-game/part-09/src/client/home.jsx
@@ -1,58 +1,50 @@
 import React from "react";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 export class Home extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if (this.props.user) {
+      this.props.fetchAndUpdateUserInfo();
     }
+  }
 
-    componentDidMount() {
-        if(this.props.user) {
-            this.props.fetchAndUpdateUserInfo();
-        }
-    }
+  render() {
+    const user = this.props.user;
 
+    return (
+      <div>
+        <div>
+          <h2>Quiz Game</h2>
 
-    render() {
-        const user = this.props.user;
+          <p>
+            Welcome to the Quiz Game! In this game, you will get a series of
+            questions, each one with 4 possible answers. Only 1 out of the 4
+            answers is correct. If you answer wrongly to any of the questions,
+            you lose! You win if you manage to answer correctly to all
+            questions.
+          </p>
 
-        return (
-            <div>
-                <div>
-                    <h2>Quiz Game</h2>
+          <p>Number of online players: {this.props.userCount}</p>
+        </div>
 
-                    <p>
-                        Welcome to the Quiz Game!
-                        In this game, you will get a series of questions, each one with 4 possible answers.
-                        Only 1 out of the 4 answers is correct.
-                        If you answer wrongly to any of the questions, you lose!
-                        You win if you manage to answer correctly to all questions.
-                    </p>
-
-                    <p>
-                        Number of online players: {this.props.userCount}
-                    </p>
-                </div>
-
-
-                {user ? (
-                    <div>
-                        <div className="btnPart">
-                            <Link to={"/match"} className={"btn"}>
-                                Play
-                            </Link>
-                        </div>
-                        <p>Victories: {user.victories}</p>
-                        <p>Defeats: {user.defeats}</p>
-                    </div>
-                ) : (
-                    <p>
-                        You need to log-in to start playing!
-                    </p>
-                )}
-
+        {user ? (
+          <div>
+            <div className="btnPart">
+              <Link to={"/match"} className={"btn"}>
+                Play
+              </Link>
             </div>
-        );
-    }
+            <p>Victories: {user.victories}</p>
+            <p>Defeats: {user.defeats}</p>
+          </div>
+        ) : (
+          <p>You need to log-in to start playing!</p>
+        )}
+      </div>
+    );
+  }
 }
